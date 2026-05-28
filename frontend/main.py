3748b78d46b62a457da77485642f3bf1d019d6ba
Dragos Radu <dragosrdy11@yahoo.com>
2026-05-28 14:42:13 +0300

    DEVOPS-59-connect-weather-agenda-backend-frontend
    Signed-off-by: Dragos Radu <dragosrdy11@yahoo.com>

--- frontend/main.py
+++ frontend/main.py
@@ -6,6 +6,8 @@ from PySide6.QtQml import QQmlApplicationEngine
 
 from controllers.system_metrics_controller import SystemMetricsController
 from controllers.local_system_info_controller import LocalSystemInfoController
+from controllers.weather_controller import WeatherController
+from controllers.calendar_controller import CalendarController
 
 
 os.environ.setdefault("QT_QUICK_CONTROLS_STYLE", "Basic")
@@ -18,9 +20,13 @@ def main():
 
     system_metrics = SystemMetricsController()
     local_system_info = LocalSystemInfoController()
+    weather = WeatherController()
+    calendar = CalendarController()
 
     engine.rootContext().setContextProperty("systemMetrics", system_metrics)
     engine.rootContext().setContextProperty("localSystemInfo", local_system_info)
+    engine.rootContext().setContextProperty("weatherController", weather)
+    engine.rootContext().setContextProperty("calendarController", calendar)
 
     engine.load("qml/Main.qml")
 
